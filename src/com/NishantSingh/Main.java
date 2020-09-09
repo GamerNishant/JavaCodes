be287b9d7bf2e97ed606063ea88dbd4a982bd26d
GamerNishant <official007nishant@gmail.com>
2020-09-09 08:31:35 +0530

    Modifying Arrays

---
+++ src/com/NishantSingh/Main.java
@@ -0,0 +1,23 @@
+package com.NishantSingh;
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] numbers = new int [5];
+
+        numbers[1] = 1;
+        numbers[0] = 0;
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Nishant Singh");
+        Point point1 = new Point(1,1);
+        System.out.println(point1);
+
+
+    }
+}
+
+
